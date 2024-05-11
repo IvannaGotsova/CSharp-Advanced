f5d488bf15a5f17d527a40e032b4dda9372c5b07
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-11 19:59:57 +0300

    Generics

---
+++ CSharp Advanced/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Advanced
+{
+    public class Employee<T>
+    {
+
+        public void GetFullName(string firstName, string lastName)
+        {
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+        public void GetFullInfo(string firstName, string lastName, int age)
+        {
+            Console.WriteLine($"{firstName} {lastName}, {age} years old.");
+        }
+        public void GetGreeting(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello from {firstName} {lastName}");
+        }
+        public void GetBonus(int experience, double salary)
+        {
+            double bonus = experience * (salary * 0.1);
+            Console.WriteLine(bonus);
+        }
+    }
+}
--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -4,11 +4,23 @@
     {
         static void Main(string[] args)
         {
-            //Extension method 
-            ClassToBeExtended classToBeExtended = new ClassToBeExtended();
-            classToBeExtended.FourthMethod();
-            classToBeExtended.FifthMethod();
-            classToBeExtended.SixthMethod();
+            //Generics
+            Employee<SeniorEmployee> seniorEmployee = new Employee<SeniorEmployee>();
+            seniorEmployee.GetFullName("Test", "Test");
+            seniorEmployee.GetFullInfo("Test", "Test", 50);
+            seniorEmployee.GetGreeting("Test", "Test");
+            seniorEmployee.GetBonus(10, 10000);
+            Employee<JuniorEmployee> juniorEmployee = new Employee<JuniorEmployee>();
+            juniorEmployee.GetFullName("Test", "Test");
+            juniorEmployee.GetFullInfo("Test", "Test", 30);
+            juniorEmployee.GetGreeting("Test", "Test");
+            juniorEmployee.GetBonus(5, 5000);
+
+            ////Extension method 
+            //ClassToBeExtended classToBeExtended = new ClassToBeExtended();
+            //classToBeExtended.FourthMethod();
+            //classToBeExtended.FifthMethod();
+            //classToBeExtended.SixthMethod();
 
             ////static keyword 
             //Person person = new Person();
