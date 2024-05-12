519e0cccbc94f785841031c2f155d9be56421a1b
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-12 14:39:14 +0300

    Delegates

--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -1,20 +1,57 @@
-﻿namespace CSharp_Advanced
+﻿using System;
+
+namespace CSharp_Advanced
 {
     internal class Program
     {
+        public delegate void MyDelegateExample(string delegateStringExample);
+
         static void Main(string[] args)
         {
-            //Generics
-            Employee<SeniorEmployee> seniorEmployee = new Employee<SeniorEmployee>();
-            seniorEmployee.GetFullName("Test", "Test");
-            seniorEmployee.GetFullInfo("Test", "Test", 50);
-            seniorEmployee.GetGreeting("Test", "Test");
-            seniorEmployee.GetBonus(10, 10000);
-            Employee<JuniorEmployee> juniorEmployee = new Employee<JuniorEmployee>();
-            juniorEmployee.GetFullName("Test", "Test");
-            juniorEmployee.GetFullInfo("Test", "Test", 30);
-            juniorEmployee.GetGreeting("Test", "Test");
-            juniorEmployee.GetBonus(5, 5000);
+            //Delegates
+            MyDelegateExample delegateExample = FirstMethodExample;
+            delegateExample("First example");
+            delegateExample = SecondMethodExample;
+            delegateExample("Second example");
+            delegateExample = ThirdMethodExample;
+            delegateExample("Third example");
+            delegateExample = FourthMethodExample;
+            delegateExample("Fourth example");
+            delegateExample = FifthMethodExample;
+            delegateExample("Fifth example");
+
+            static void FirstMethodExample(string example)
+            {
+                Console.WriteLine(example);
+            }
+            static void SecondMethodExample(string example)
+            {
+                Console.WriteLine(example);
+            }
+            static void ThirdMethodExample(string example)
+            {
+                Console.WriteLine(example);
+            }
+            static void FourthMethodExample(string example)
+            {
+                Console.WriteLine(example);
+            }
+            static void FifthMethodExample(string example)
+            {
+                Console.WriteLine(example);
+            }
+
+            ////Generics
+            //Employee<SeniorEmployee> seniorEmployee = new Employee<SeniorEmployee>();
+            //seniorEmployee.GetFullName("Test", "Test");
+            //seniorEmployee.GetFullInfo("Test", "Test", 50);
+            //seniorEmployee.GetGreeting("Test", "Test");
+            //seniorEmployee.GetBonus(10, 10000);
+            //Employee<JuniorEmployee> juniorEmployee = new Employee<JuniorEmployee>();
+            //juniorEmployee.GetFullName("Test", "Test");
+            //juniorEmployee.GetFullInfo("Test", "Test", 30);
+            //juniorEmployee.GetGreeting("Test", "Test");
+            //juniorEmployee.GetBonus(5, 5000);
 
             ////Extension method 
             //ClassToBeExtended classToBeExtended = new ClassToBeExtended();
@@ -39,7 +76,7 @@
             //int age = person.Age = 1;
             //Console.WriteLine($"full name: {firstname} {lastname}, age: {age}, number: {person.number}");
 
-
         }
     }
+    
 }
