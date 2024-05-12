ea1dcae95f87280d2b040ba5e753eff30c28a6a1
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-12 20:13:30 +0300

    Yield

--- CSharp Advanced/Program.cs
+++ CSharp Advanced/Program.cs
@@ -9,7 +9,12 @@ namespace CSharp_Advanced
 
         static void Main(string[] args)
         {
-     
+            //yield
+            foreach (var number in GetNumbers())
+            {
+                Console.WriteLine(number);
+            }
+
             //Asynchronous programming
 
             //FirstProcess();
@@ -114,63 +119,71 @@ namespace CSharp_Advanced
             //Console.WriteLine($"full name: {firstname} {lastname}, age: {age}, number: {person.number}");
 
         }
-            //private static void FifthProcess()
-            //{
-            //    Console.WriteLine("Fifth Process begin");
-            //    Console.WriteLine("Fifth Process stop");
-            //}
 
-            //private static async void FourthProcess()
-            //{
-            //    await Task.Delay(1000);
-            //    Console.WriteLine("Fourth Process begin");
-            //    Console.WriteLine("Fourth Process stop");
-            //}
-
-            //private static async void ThirdProcess()
-            //{
-            //    Console.WriteLine("Third Process begin");
-            //    await Task.Delay(5000);
-            //    Console.WriteLine("Third Process stop");
-            //}
-
-            //private static async void SecondProcess()
-            //{
-            //    Console.WriteLine("Second Process begin");
-            //    await Task.Delay(10000);
-            //    Console.WriteLine("Second Process stop");
-            //}
-
-            //private static void FirstProcess()
-            //{
-            //    Console.WriteLine("First Process begin");
-            //    Console.WriteLine("First Process stop");
-            //}
-
-            //private static void EventOnExperienceDecreased(object? sender, int e)
-            //{
-            //    var juniorEmployee = sender as JuniorEmployee;
-            //    Console.WriteLine("Experience increased");
-            //}
-
-            //private static void EventOnExperienceIncreased(object? sender, int e)
-            //{
-            //    var juniorEmployee = sender as JuniorEmployee;
-            //    Console.WriteLine("Experience decreased");
-            //}
-
-            //private static void EventOnSalaryDecreaased(object? sender, decimal salary)
-            //{
-            //    var seniorEmployee = sender as SeniorEmployee;
-            //    Console.WriteLine("Salary increased");
-            //}
-
-            //private static void EventOnSalaryIncreaased(object? sender, decimal salary)
-            //{
-            //    var seniorEmployee = sender as SeniorEmployee;
-            //    Console.WriteLine("Salary decreased");
-            //}
-
+        private static IEnumerable<object> GetNumbers()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                yield return i;
+            }
         }
+        //private static void FifthProcess()
+        //{
+        //    Console.WriteLine("Fifth Process begin");
+        //    Console.WriteLine("Fifth Process stop");
+        //}
+
+        //private static async void FourthProcess()
+        //{
+        //    await Task.Delay(1000);
+        //    Console.WriteLine("Fourth Process begin");
+        //    Console.WriteLine("Fourth Process stop");
+        //}
+
+        //private static async void ThirdProcess()
+        //{
+        //    Console.WriteLine("Third Process begin");
+        //    await Task.Delay(5000);
+        //    Console.WriteLine("Third Process stop");
+        //}
+
+        //private static async void SecondProcess()
+        //{
+        //    Console.WriteLine("Second Process begin");
+        //    await Task.Delay(10000);
+        //    Console.WriteLine("Second Process stop");
+        //}
+
+        //private static void FirstProcess()
+        //{
+        //    Console.WriteLine("First Process begin");
+        //    Console.WriteLine("First Process stop");
+        //}
+
+        //private static void EventOnExperienceDecreased(object? sender, int e)
+        //{
+        //    var juniorEmployee = sender as JuniorEmployee;
+        //    Console.WriteLine("Experience increased");
+        //}
+
+        //private static void EventOnExperienceIncreased(object? sender, int e)
+        //{
+        //    var juniorEmployee = sender as JuniorEmployee;
+        //    Console.WriteLine("Experience decreased");
+        //}
+
+        //private static void EventOnSalaryDecreaased(object? sender, decimal salary)
+        //{
+        //    var seniorEmployee = sender as SeniorEmployee;
+        //    Console.WriteLine("Salary increased");
+        //}
+
+        //private static void EventOnSalaryIncreaased(object? sender, decimal salary)
+        //{
+        //    var seniorEmployee = sender as SeniorEmployee;
+        //    Console.WriteLine("Salary decreased");
+        //}
+
+    }
 
     }
