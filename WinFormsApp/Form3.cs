99d33b5ac551a26959e5afefc41ff68237743977
IvannaGotsova <ivannagotsova@outlook.com>
2025-01-26 12:22:53 +0200

    Extendet conditions

--- WinFormsApp/Form3.cs
+++ WinFormsApp/Form3.cs
@@ -19,13 +19,43 @@ namespace WinFormsApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (int.Parse(textBox4.Text) >= 18)
+            bool isRight = true;
+
+            if (textBox1.Text.Length < 2 || textBox1.Text.Length > 99)
             {
-                textBox6.Text = $"The full name is {textBox1.Text} {textBox2.Text} {textBox3.Text}. the age is {textBox4.Text}. The city is {textBox5.Text}.";
+                textBox1.Text = "Enter First Name";
+                isRight = false;
             }
-            else
+
+            if (textBox2.Text.Length < 2 || textBox2.Text.Length > 99)
+            {
+                textBox2.Text = "Enter Middle Name";
+                isRight = false;
+            }
+
+            if (textBox3.Text.Length < 2 || textBox3.Text.Length > 99)
+            {
+                textBox3.Text = "Enter Last Name";
+                isRight = false;
+            };
+
+            int age  = int.Parse(textBox4.Text);
+
+            if (age > 2 || age < 100)
             {
                 textBox4.Text = "Enter age";
+                isRight = false;
+            }
+
+            if (textBox5.Text.Length < 2 || textBox5.Text.Length > 20)
+            {
+                textBox5.Text = "Enter City";
+                isRight = false;
+            }
+
+            if (isRight)
+            {
+                textBox6.Text = $"The full name is {textBox1.Text} {textBox2.Text} {textBox3.Text}. the age is {textBox4.Text}. The city is {textBox5.Text}.";
             }
         }
     }
